7fda485bf89161b9b124a4fad319c64bb866a48a
Yashvanth2402 <yashvanthbalaji2402@gmail.com>
2026-01-31 12:41:26 +0530

    Update outputs.tf

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,24 +1,8 @@
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.rg.name
+output "ssh_private_key" {
+  value     = tls_private_key.vm_ssh.private_key_pem
+  sensitive = true
 }
 
-output "virtual_network_name" {
-  description = "Name of the virtual network"
-  value       = azurerm_virtual_network.vnet.name
-}
-
-output "subnet_name" {
-  description = "Name of the subnet"
-  value       = azurerm_subnet.subnet.name
-}
-
-output "vm_name" {
-  description = "Name of the virtual machine"
-  value       = azurerm_linux_virtual_machine.vm.name
-}
-
-output "vm_private_ip" {
-  description = "Private IP address of the VM"
-  value       = azurerm_network_interface.nic.private_ip_address
+output "ssh_public_key" {
+  value = tls_private_key.vm_ssh.public_key_openssh
 }
