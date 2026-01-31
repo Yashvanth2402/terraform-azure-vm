8b9a7770492b6c229795fe70849b887d60d3172e
yashvanth2402 <yashvanthbalaji2498@gmail.com>
2026-01-31 11:39:21 +0530

    test: Update VM size for PR testing

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "example-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2s"
+  size                = "Standard_B4ms"
   admin_username      = "azureuser"
 
   network_interface_ids = [
