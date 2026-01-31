6fea4d68f0bc64b55c372d4ad7580fca18b36bd0
Yashvanth2402 <yashvanthbalaji2402@gmail.com>
2026-01-31 15:02:50 +0530

    Update variables.tf

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,27 +1,24 @@
 variable "location" {
-  description = "Azure region for resources"
-  type        = string
-  default     = "East US"
+  type    = string
+  default = "East US"
 }
 
-variable "vm_size" {
-  description = "Size of the virtual machine"
-  type        = string
-  default     = "Standard_B4ms"
+variable "resource_group_name" {
+  type = string
 }
 
-variable "data_disk_size" {
-  description = "Size of data disk in GB"
-  type        = number
-  default     = 64
-  validation {
-    condition     = var.data_disk_size >= 32 && var.data_disk_size <= 4096
-    error_message = "Data disk size must be between 32GB and 4096GB."
-  }
+variable "vnet_name" {
+  type = string
 }
 
-variable "allowed_ssh_cidr" {
-  description = "CIDR block allowed for SSH access"
-  type        = string
-  default     = "10.0.0.0/24"
+variable "subnet_name" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
 }
