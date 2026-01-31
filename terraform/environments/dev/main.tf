5b2850ef7a3bf96f3732979bcb470a34af28d413
Yashvanth2402 <yashvanthbalaji2402@gmail.com>
2026-01-31 14:59:14 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,149 +1,18 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-vm-dev"
+  name     = var.resource_group_name
   location = var.location
-  
-  tags = {
-    Environment = "dev"
-    CostCenter  = "IT"
-    ManagedBy   = "Terraform"
-  }
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-dev"
-  address_space       = ["10.1.0.0/16"]
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "vm-subnet"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.1.1.0/24"]
-}
-
-resource "azurerm_network_security_group" "vm_nsg" {
-  name                = "vm-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "AllowSSH"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "10.0.0.0/24"  # Restricted to internal network
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "AllowHTTP"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "Internet"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "DenyAll"
-    priority                   = 4000
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_subnet_network_security_group_association" "example" {
-  subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.vm_nsg.id
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "vm-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "example-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = var.vm_size
-  admin_username      = "azureuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-
-  # No SSH key - using password authentication for testing
-  disable_password_authentication = false
-  
-  tags = {
-    Environment = "dev"
-    Application = "webserver"
-  }
-}
-
-resource "azurerm_managed_disk" "data_disk" {
-  name                 = "vm-data-disk"
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = var.data_disk_size
-  
-  tags = {
-    Environment = "dev"
-    Purpose     = "DataStorage"
-  }
-}
-
-resource "azurerm_virtual_machine_data_disk_attachment" "example" {
-  virtual_machine_id = azurerm_linux_virtual_machine.vm.id
-  managed_disk_id    = azurerm_managed_disk.data_disk.id
-  lun                = "10"
-  caching            = "ReadWrite"
+  address_prefixes     = ["10.0.1.0/24"]
 }
