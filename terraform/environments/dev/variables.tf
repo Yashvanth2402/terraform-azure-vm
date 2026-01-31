03557823a26ad705185f96df11ab53e9861defca
Yashvanth2402 <yashvanthbalaji2402@gmail.com>
2026-01-31 14:43:06 +0530

    Update variables.tf

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -10,8 +10,18 @@ variable "vm_size" {
   default     = "Standard_B4ms"
 }
 
-variable "ssh_public_key" {
-  description = "SSH public key for VM access"
+variable "data_disk_size" {
+  description = "Size of data disk in GB"
+  type        = number
+  default     = 64
+  validation {
+    condition     = var.data_disk_size >= 32 && var.data_disk_size <= 4096
+    error_message = "Data disk size must be between 32GB and 4096GB."
+  }
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed for SSH access"
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC..."
+  default     = "10.0.0.0/24"
 }
