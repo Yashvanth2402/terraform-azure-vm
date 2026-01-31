d66298b69d13b0dc35439e4b038e965e5af742dd
Yashvanth2402 <yashvanthbalaji2402@gmail.com>
2026-01-31 14:42:25 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -14,6 +14,12 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "rg-vm-dev"
   location = var.location
+  
+  tags = {
+    Environment = "dev"
+    CostCenter  = "IT"
+    ManagedBy   = "Terraform"
+  }
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -28,6 +34,53 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "vm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "10.0.0.0/24"  # Restricted to internal network
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "DenyAll"
+    priority                   = 4000
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -65,8 +118,32 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = var.ssh_public_key
+  # No SSH key - using password authentication for testing
+  disable_password_authentication = false
+  
+  tags = {
+    Environment = "dev"
+    Application = "webserver"
   }
+}
+
+resource "azurerm_managed_disk" "data_disk" {
+  name                 = "vm-data-disk"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = var.data_disk_size
+  
+  tags = {
+    Environment = "dev"
+    Purpose     = "DataStorage"
+  }
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  virtual_machine_id = azurerm_linux_virtual_machine.vm.id
+  managed_disk_id    = azurerm_managed_disk.data_disk.id
+  lun                = "10"
+  caching            = "ReadWrite"
 }
